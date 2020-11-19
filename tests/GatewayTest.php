b5822e5f5d8fa4e8e30dd8ce3cbd0d632c008cc4
yildiray.itisgen <yildiray.itisgen@alegradigital.com>
2020-11-19 15:49:58 +0300

    Added testcases.

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace OmnipayTest\PayU;
+namespace OmnipayTest\gvp;
 
 use Omnipay\Gvp\Gateway;
+
 use Omnipay\Gvp\Messages\AuthorizeRequest;
 use Omnipay\Gvp\Messages\CaptureRequest;
-use Omnipay\Gvp\Messages\CompletePurchaseRequest;
 use Omnipay\Gvp\Messages\PurchaseRequest;
 use Omnipay\Gvp\Messages\RefundRequest;
 use Omnipay\Gvp\Messages\VoidRequest;
@@ -21,54 +21,45 @@ class GatewayTest extends GatewayTestCase
     public function testPurchase(): void
     {
         /** @var PurchaseRequest $request */
-        $request = $this->gateway->purchase(['orderRef' => '41838239']);
+        $request = $this->gateway->purchase(['orderId' => '41838239']);
 
         self::assertInstanceOf(PurchaseRequest::class, $request);
-        self::assertSame('41838239', $request->getOrderRef());
-    }
-
-    public function testCompletePurchase(): void
-    {
-        /** @var CompletePurchaseRequest $request */
-        $request = $this->gateway->completePurchase(['orderRef' => '41838239']);
-
-        self::assertInstanceOf(CompletePurchaseRequest::class, $request);
-        self::assertSame('41838239', $request->getOrderRef());
+        self::assertSame('41838239', $request->getOrderId());
     }
 
     public function testAuthorize(): void
     {
         /** @var AuthorizeRequest $request */
-        $request = $this->gateway->authorize(['bin' => '557829']);
+        $request = $this->gateway->authorize(['orderId' => '41838239']);
 
-        self::assertInstanceOf(CardInfoV1Request::class, $request);
-        self::assertSame('557829', $request->getBin());
+        self::assertInstanceOf(AuthorizeRequest::class, $request);
+        self::assertSame('41838239', $request->getOrderId());
     }
 
     public function testCapture(): void
     {
-        /** @var CaptureRequest $request */
-        $request = $this->gateway->capture(['clientId' => '34353']);
+        /** @var AuthorizeRequest $request */
+        $request = $this->gateway->capture(['orderId' => '41838239']);
 
         self::assertInstanceOf(CaptureRequest::class, $request);
-        self::assertSame('34353', $request->getClientId());
+        self::assertSame('41838239', $request->getOrderId());
     }
 
     public function testRefund(): void
     {
         /** @var RefundRequest $request */
-        $request = $this->gateway->refund(['orderRef' => '41838239']);
+        $request = $this->gateway->refund(['orderId' => '41838239']);
 
         self::assertInstanceOf(RefundRequest::class, $request);
-        self::assertSame('41838239', $request->getOrderRef());
+        self::assertSame('41838239', $request->getOrderId());
     }
 
     public function testVoid(): void
     {
         /** @var VoidRequest $request */
-        $request = $this->gateway->void(['refNoExt' => '784']);
+        $request = $this->gateway->void(['orderId' => '41838239']);
 
         self::assertInstanceOf(VoidRequest::class, $request);
-        self::assertSame('784', $request->getRefNoExt());
+        self::assertSame('41838239', $request->getOrderId());
     }
 }
